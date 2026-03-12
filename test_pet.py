48410a56331175bcf3750649f254271112bebc41
Sachin Kherde <skherde@gmail.com>
2026-03-12 13:29:02 -0700

    Fixed schema and implemented APIs as per TODO tasks

--- test_pet.py
+++ test_pet.py
@@ -1,4 +1,4 @@
-from jsonschema import validate
+from jsonschema import validate, ValidationError
 import pytest
 import schemas
 import api_helpers
@@ -14,10 +14,17 @@ def test_pet_schema():
 
     response = api_helpers.get_api_data(test_endpoint)
 
-    assert response.status_code == 200
+    assert response.status_code == 200, f"Ecpected 200 , got {response.status_code}"
+
+    data = response.json()
+
 
     # Validate the response schema against the defined schema in schemas.py
-    validate(instance=response.json(), schema=schemas.pet)
+    try:
+        validate(instance=data, schema=schemas.pet)
+    except ValidationError as e:
+        print("Schema validation error", e.message)
+        assert False, f"Schema validation failed: {e.message}"
 
 '''
 TODO: Finish this test by...
@@ -26,7 +33,7 @@ TODO: Finish this test by...
 3) Validate the 'status' property in the response is equal to the expected status
 4) Validate the schema for each object in the response
 '''
-@pytest.mark.parametrize("status", [("available")])
+@pytest.mark.parametrize("status", ["available", "sold", "pending"])
 def test_find_by_status_200(status):
     test_endpoint = "/pets/findByStatus"
     params = {
@@ -34,7 +41,12 @@ def test_find_by_status_200(status):
     }
 
     response = api_helpers.get_api_data(test_endpoint, params)
-    # TODO...
+    assert response.status_code ==200, f"Expected 200, got {response.status_code}"
+
+    pets = response.json()
+    for pet in pets:
+        assert pet["status"] == status, f"Expected status '{status}', got '{pet['status']}'"
+        validate (instance=pet, schema=schemas.pet)
 
 '''
 TODO: Finish this test by...
@@ -42,5 +54,11 @@ TODO: Finish this test by...
 2) Parameterizing the test for any edge cases
 '''
 def test_get_by_id_404():
-    # TODO...
-    pass
+    
+    endpoint = f"/pets/10"
+    response = api_helpers.get_api_data(endpoint)
+
+    assert response.status_code == 404 , f" Expected 404 for per={pet_id}, got (response.status_code)"
+    data = response.json()
+    if "message" in data:
+        print(f" API reponse message for pet_id = (pet_id): (data('message'))")
--- test_store.py
+++ test_store.py
@@ -12,5 +12,23 @@ TODO: Finish this test by...
 3) Validate the response codes and values
 4) Validate the response message "Order and pet status updated successfully"
 '''
-def test_patch_order_by_id():
-    pass
+
+    @pytest.fixture
+    def test_order_id():
+
+        return 1 #In real world,we can use post API to create new order and return ID
+
+    def test_patch_order_by_id(test_order_id):
+    
+    endpoint = f"/store/order/(test_order_id)"
+    payload = ("status": "sold")
+
+    response = api_helpers.patch_api_data(endpoint, payload)
+    assert response.status_code == 200 , f"Expected 200, got (response.status_code)"
+
+    body = response.json
+
+    assert body.get("message") == "Order and pat status updated successfully", f"Unexpected message: (body.get('message))"
+
+    assert body.get("status") == "sold", f" Expected status 'sold, got (body.get('status'))"
+
